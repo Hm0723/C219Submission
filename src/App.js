bcdb0511b40f67c17c622901b05c13be05fe11c1
Ng Haw Ming <23037176@myrp.edu.sg>
2025-01-02 19:29:04 +0800

    1

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+const KEY = "a133b2aa";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+
+    const controller = new AbortController();
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal })
+      .then((res) => res.json())
+      .then((data) => data.Response === "True" && setMovies(data.Search))
+      .catch((err) => console.log(err));
+    return () => controller.abort();
+  }, [query]);
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <input type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)}/>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.imdbID}>
+              <td>{movie.Title}</td>
+              <td>{movie.Year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
